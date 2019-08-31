3f8caecc577c55f0a910aff0091437ca21a7d131
sanukin39 <sanuki@mini.local>
2019-08-31 23:41:08 +0900

    Supprot Notification For Android O and later

--- AndroidLibraryProject/unilocalnotification/src/main/java/net/sanukin/unilocalnotification/NotificationReceiver.java
+++ AndroidLibraryProject/unilocalnotification/src/main/java/net/sanukin/unilocalnotification/NotificationReceiver.java
@@ -1,6 +1,7 @@
 package net.sanukin.unilocalnotification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -11,6 +12,8 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 
 /**
@@ -50,33 +53,58 @@ public class NotificationReceiver extends BroadcastReceiver {
         final int appIconResId=applicationInfo.icon;
         Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), appIconResId);
 
-        // create notification builder
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
-        builder.setContentIntent(contentIntent);
-
-        // set notification info
-        builder.setTicker("");
-        builder.setContentTitle(title);
-        builder.setContentText(message);
-
-        // set icon
-        builder.setLargeIcon(largeIcon);
-        builder.setSmallIcon(context.getResources().getIdentifier("notification_icon", "drawable", context.getPackageName()));
-
-        // fire now
-        builder.setWhen(System.currentTimeMillis());
-
-        // set device notification settings
-        builder.setDefaults(Notification.DEFAULT_SOUND
-                | Notification.DEFAULT_VIBRATE
-                | Notification.DEFAULT_LIGHTS);
-
-        // tap to cancel
-        builder.setAutoCancel(true);
-
-        // fire notification
         NotificationManager manager = (NotificationManager) context.getSystemService(Service.NOTIFICATION_SERVICE);
-        manager.notify(requestCode, builder.build());
-    }
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            String channelId = "default";
+            NotificationChannel channel = new NotificationChannel(channelId, title, NotificationManager.IMPORTANCE_DEFAULT);
+
+            channel.setDescription(message);
+            channel.enableVibration(true);
+            channel.canShowBadge();
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            channel.setShowBadge(true);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+
+            Notification notification = new Notification.Builder(context, channelId)
+                    .setContentTitle(title)
+                    .setContentText(message)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(context.getResources().getIdentifier("notification_icon", "drawable", context.getPackageName()))
+                    .setLargeIcon(largeIcon)
+                    .build();
+
+            manager.notify(requestCode, notification);
+        } else {
+            // create notification builder
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+            builder.setContentIntent(contentIntent);
+
+            // set notification info
+            builder.setTicker("");
+            builder.setContentTitle(title);
+            builder.setContentText(message);
+
+            // set icon
+            builder.setLargeIcon(largeIcon);
+            builder.setSmallIcon(context.getResources().getIdentifier("notification_icon", "drawable", context.getPackageName()));
+
+            // fire now
+            builder.setWhen(System.currentTimeMillis());
+
+            // set device notification settings
+            builder.setDefaults(Notification.DEFAULT_SOUND
+                    | Notification.DEFAULT_VIBRATE
+                    | Notification.DEFAULT_LIGHTS);
+
+            // tap to cancel
+            builder.setAutoCancel(true);
+
+            // fire notification
+            manager.notify(requestCode, builder.build());
+        }
+    }
 }
